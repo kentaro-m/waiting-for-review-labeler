c4359ffbdd4a9c21f9e0567d78487b691738804b
kentaro-m <kentaro.m811@gmail.com>
2022-01-09 01:20:33 +0900

    feat: add implementation to add label after waiting time for review

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,96 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import type {GraphQlQueryResponse} from '@octokit/graphql/dist-types/types'
+import dayjs from 'dayjs'
+
+type QueryResponse = {
+  search: {
+    nodes: PullRequest[] | []
+  }
+}
+
+type PullRequest = {
+  number: number
+  createdAt: string
+  timelineItems: {
+    nodes:
+      | {
+          createdAt: string
+        }[]
+      | []
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const hoursBeforeLabelAdd = core.getInput('hours-before-label-add', {
+      required: true
+    })
+    const labelName = core.getInput('label-name', {
+      required: true
+    })
+    const token = core.getInput('repo-token', {required: true})
+    const octokit = github.getOctokit(token)
+    const context = github.context
+    const repoWithOwner = `${context.repo.owner}/${context.repo.repo}`
+    const response = await octokit.graphql<GraphQlQueryResponse<QueryResponse>>(
+      `fragment pr on PullRequest {
+        ... on PullRequest {
+          number
+          createdAt
+          timelineItems(itemTypes: READY_FOR_REVIEW_EVENT, first: 1) {
+            nodes {
+              ... on ReadyForReviewEvent {
+                createdAt
+              }
+            }
+          }
+        }
+      }
+      
+      query ($q: String!, $limit: Int = 20) {
+        search(first: $limit, type: ISSUE, query: $q) {
+          nodes {
+            ...pr
+          }
+        }
+      }`,
+      {
+        q: `is:pr is:open draft:false repo:${repoWithOwner}`
+      }
+    )
+    const pullRequests = response.data.search.nodes
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (pullRequests.length === 0) {
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const targetPullRequests = pullRequests
+      .map(pullRequest => {
+        const createdAt =
+          pullRequest.timelineItems.nodes.length === 0
+            ? pullRequest.createdAt
+            : pullRequest.timelineItems.nodes[0].createdAt
+        const readyForReviewAt = dayjs(createdAt)
+        const diff = readyForReviewAt.diff('hour')
+
+        if (diff < parseInt(hoursBeforeLabelAdd, 10)) {
+          return
+        }
+
+        return pullRequest
+      })
+      .filter(v => v !== undefined)
+
+    for (const pullRequest of targetPullRequests) {
+      pullRequest?.number &&
+        (await octokit.rest.issues.addLabels({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          issue_number: pullRequest?.number,
+          labels: [labelName]
+        }))
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
